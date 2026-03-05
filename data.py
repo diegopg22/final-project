1e5dfeb95225b9002df8593f6c083724b21159a9
diegopg22 <dplasc03@calpoly.edu>
2026-03-05 10:47:04 -0800

    beginning

--- data.py
+++ data.py
@@ -1,16 +1,28 @@
-def __init__(self, city_name, country, population, food_waste_per_capita):
-    self.city_name = city_name
-    self.country = country
-    self.population = population
-    self.food_waste_per_capita = food_waste_per_capita
+from multiprocessing.managers import view_type
 
-def __repr__(self):
-    return "{}, {} | Population: {} | Food Waste per Capita: {}".format(
-        self.city_name,
-        self.country,
-        self.population,
-        self.food_waste_per_capita
-    )
+class city:
+    def __init__(self, city_name, country, population, food_waste_per_capita):
+        self.city_name = city_name
+        self.country = country
+        self.population = population
+        self.food_waste_per_capita = food_waste_per_capita
+
+    def __repr__(self):
+        return "{}, {} | Population: {} | Food Waste per Capita: {}".format(
+            self.city_name,
+            self.country,
+            self.population,
+            self.food_waste_per_capita
+        )
+cities = [
+    city('Rio de Janeiro', 'Brasil', 6700000, 94),
+    city('San Miguel', 'Ecuador', 2470, 34),
+    city('Punta Hermosa', 'Peru', 23470, 91),
+    city('Belize City', 'Belize', 53000, 34),
+    city('San Pedro', 'Belize',12000, 36),
+    city('Salcedo Municipality', 'Dominican Republic', )
+
+]
 
 #finds the average waste per capita per for the city
 def average_waste(self):
