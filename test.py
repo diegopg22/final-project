75cf24d3685b1cb6a795c995f89a84c24080e839
Greathax <jli350@calpoly.edu>
2026-03-09 10:16:04 -0700

    tests

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import data
 from main import average_waste, food_waste_gt, food_waste_lt, feedback
+import unittest
 
 #cities for sample testing
 
@@ -8,36 +9,42 @@ city2 = data.City('Bogota', 'Columbia', 7400000,70)
 city3 = data.City('Caye Caulker', 'Belize', 1600, 45)
 
 cities = [city1, city2, city3]
+class Testcases(unittest.TestCase):
+    pass
 
-#average waste test
-def test_average_waste():
-    assert average_waste(cities) == 72
+    #average waste test
+    def test_average_waste_1(self):
+        result = average_waste(cities)
+        expected = 72.0
+        self.assertAlmostEqual(result, expected)
 
-def test_average_waste_():
-    lst = [city1, city3]
-    assert average_waste(lst) == 73
+    def test_average_waste_2(self):
+        lst = [city1, city3]
+        result = average_waste(lst)
+        expected = 73.0
+        self.assertAlmostEqual(result, expected)
 
-#food waste test
-def test_food_waste_gt():
-    result = food_waste_gt(cities)
-    assert result == [city1]
+    #food waste test
+    def test_food_waste_gt_1(self):
+        result = food_waste_gt(cities)
+        expected = [city1]
+        self.assertEqual(result, expected)
 
-def test_food_waste_gt_():
-    lst = [city2, city3]
-    result = food_waste_gt(lst)
-    assert result == [city2]
+    def test_food_waste_gt_2(self):
+        lst = [city2, city3]
+        result = food_waste_gt(lst)
+        expected = [city2]
+        self.assertEqual(result, expected)
 
-#food waste less than
-def test_food_waste_lt():
-    result = food_waste_lt(cities)
-    assert result == [city2, city3]
+    #food waste less than
+    def test_food_waste_lt_1(self):
+        result = food_waste_lt(cities)
+        expected= [city2, city3]
+        self.assertEqual(result, expected)
 
-def test_food_waste_lt_():
-    lst = [city1]
-    result = food_waste_lt(lst)
-    assert result == [city1]
-
-#feedback test
-def test_feedback():
-    feedback([city1, city2, city3])
+    def test_food_waste_lt_2(self):
+        lst = [city1]
+        result = food_waste_lt(lst)
+        expected = [city1]
+        self.assertEqual(result, expected)
 
