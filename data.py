7119eaa53ad4092140ac340b24c58a473fa27963
Greathax <jli350@calpoly.edu>
2026-03-09 09:57:50 -0700

    Final city list

--- data.py
+++ data.py
@@ -15,14 +15,20 @@ class City:
             self.food_waste_per_capita
         )
 cities = [
-    City('Rio de Janeiro', 'Brasil', 6700000, 94),
+    City('Rio de Janeiro', 'Brazil', 6700000, 94),
     City('San Miguel', 'Ecuador', 2470, 34),
+    City('Santo Domingo', 'Ecuador', 334826, 158),
     City('Punta Hermosa', 'Peru', 23470, 91),
+    City('Macusani', 'Peru', 11707, 84),
     City('Belize City', 'Belize', 53000, 34),
     City('San Pedro', 'Belize',12000, 36),
     City('Caye Caulker', 'Belize', 1600, 45),
     City('Salcedo Municipality', 'Dominican Republic', 35000, 207),
     City('Santo Domingo', 'Dominican Republic', 4000000, 113),
     City('Panama City', 'Panama', 1300000, 101),
-    City('Bogota', 'Columbia', 7400000,70)
+    City('Bogota', 'Colombia', 7400000,70),
+    City('Mexicali', 'Mexico', 1200000, 126),
+    City('Berriozábal', 'Mexico', 64632, 71),
+    City('Ensenada', 'Mexico', 330652, 129),
+    City('Chacao', 'Venezuela', 64629, 93)
 ]
